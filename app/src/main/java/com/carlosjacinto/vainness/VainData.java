5daa0c1aeec9d906e95135efc470de8f8f3f33fd
xxxapptizing <apptizing@gmail.com>
2016-01-27 05:31:45 +0000

    Splash screen and drawable update. v2

--- app/src/main/java/com/carlosjacinto/vainness/VainData.java
+++ app/src/main/java/com/carlosjacinto/vainness/VainData.java
@@ -11,8 +11,8 @@ public class VainData {
             "Rotation"
     };
     static String[] Articles = {
-            "Heroes\n\nAdagio",
-            "Items\n\namet nibh.",
-            "Rotation\n\nnibh."
+            "Heroes\n\nAdagio\n\nCatherine\n\nGlaive\n\nJoule\n\nKoshka\n\nKrul\n\nPetal\n\nRingo\n\nSaw",
+            "Items\n\nAegis\n\nAftershock\n\nAlternating Current\n\nAtlas Pauldron\n\nBarbed Needle\n\nBlazing Salvo\n\nBonesaw",
+            "Rotation\n\nCeleste\n\nFortress\n\nJoule\n\nScarf\n\nPhinn\n\nReim\n\nRingo"
     };
 }
